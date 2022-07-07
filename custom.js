a0ec392699ceee1c3b7f4d491e8cbe8504c886ca
eych-en <75099114+eych-en@users.noreply.github.com>
2022-07-07 11:14:15 +0800

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,54 @@
+
+const part1 = function() {
+	$('#part1Error').addClass('d-none');
+	$('#part1Success').addClass('d-none');
+	$.ajax({
+		url: "test.html",
+		method: "POST",
+		data: {
+			"Name": $('#part1Name').val(),
+			"Email": $('#part1Email').val(),
+			"Mobile": "0" + $('#part1Mobile').val(),
+			"PositionApplied": $('#part1PositionApplied').val(),
+			"Source": $('#part1Source').val(),
+		},
+		contentType: "application/json",
+	}).done(function(d,s,x) {
+		$('#part1Success').removeClass('d-none');
+		$('#part1Success').html(d);
+		console.log(d);
+	}).fail(function(x,s,e) {
+		$('#part1Error').removeClass('d-none');
+		console.log(s);
+	});
+}
+
+const validateMobile = function(obj) {
+	obj.val(obj.val().replace(/[^0-9]/g, ''));
+}
+
+$(document).on("scroll", function(){
+	if
+		($(document).scrollTop() > 86){
+		$("#banner").addClass("shrink");
+	}
+	else
+	{
+		$("#banner").removeClass("shrink");
+	}
+});
+
+/*
+$.ajax({
+  url: "test.html",
+	method: "POST",
+	data: [],
+	contentType: "application/json",
+}).done(function(d,s,x) {
+  
+}).fail(function(x,s,e) {
+  
+}).always(function(d,s,x) {
+  
+});
+*/
